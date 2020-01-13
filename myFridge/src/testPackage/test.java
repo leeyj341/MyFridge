fe56e57b94f4d8ef8d48ee89450d6caede3ea35a
So-Youn <soyun3963@gmail.com>
2020-01-13 16:59:23 +0900

    * 소윤 테스트

--- myFridge/src/testPackage/test.java
+++ myFridge/src/testPackage/test.java
@@ -3,7 +3,7 @@ package testPackage;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("영주머지테스트");
+		System.out.println("소윤머지테스트");
 		System.out.println("소윤머지");
 		System.out.println("영주메롱메롱");
 	}
