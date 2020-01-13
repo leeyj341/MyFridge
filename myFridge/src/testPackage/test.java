1758a6bf1436904e6815ec0b8ad2f7e0331bf3c8
leeyj341 <leeyj3794@gmail.com>
2020-01-13 17:01:38 +0900

    * 머지 충돌 테스트

--- myFridge/src/testPackage/test.java
+++ myFridge/src/testPackage/test.java
@@ -3,8 +3,6 @@ package testPackage;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("영주머지테스트");
-		System.out.println("소윤머지");
-
+		System.out.println("머지충돌테스트");
 	}
 }
