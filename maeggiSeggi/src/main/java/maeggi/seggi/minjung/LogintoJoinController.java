f04367be555c9c8a98102c616350c9d0256d4947
snowcuphea <snowcuphea@gmail.com>
2020-01-30 22:08:51 +0900

    김민정 login to join controller

--- maeggiSeggi/src/main/java/maeggi/seggi/minjung/LogintoJoinController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/minjung/LogintoJoinController.java
@@ -1,5 +1,15 @@
 package maeggi.seggi.minjung;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LogintoJoinController {
+	
+@RequestMapping("/logintojoin.do")	
+public String logintojoin() {
+	
+	return "logintojoin";
+}
 
 }
