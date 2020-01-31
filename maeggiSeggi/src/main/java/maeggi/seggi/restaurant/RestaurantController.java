7a961860088f7ac2db4f9f3bb9788f7c2adf44ae
jm <jm@DESKTOP-RFQ15RC>
2020-01-31 17:05:55 +0900

    지도, controller

--- maeggiSeggi/src/main/java/maeggi/seggi/restaurant/RestaurantController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/restaurant/RestaurantController.java
@@ -1,5 +1,17 @@
 package maeggi.seggi.restaurant;
 
-public class RestaurantController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class RestaurantController {
+	@RequestMapping("/restaurant.do")
+	public String restaurantview() {
+		return "restaurant";
+	}
+	
+	@RequestMapping("/map.do")
+	public String mapview() {
+		return "map";
+	}
 }
