47b9a747df8ccb960c325c6919c72f48186a54a5
snowcuphea <snowcuphea@gmail.com>
2020-02-05 09:11:00 +0900

    * 김민정 로그인 구현 재확인 commit

--- maeggiSeggi/src/main/java/maeggi/seggi/loginandcustomer/loginandcustomerController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/loginandcustomer/loginandcustomerController.java
@@ -1,16 +1,55 @@
 package maeggi.seggi.loginandcustomer;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class loginandcustomerController {
-
-	@RequestMapping("/loginandcustomer/login.do")
-	public String login() {
-
+	@Autowired
+	memberService service;
+	
+	
+	@RequestMapping(value= "/loginandcustomer/login.do", method = RequestMethod.GET)
+	public String loginPage() {
 		return "loginandcustomer/login";
 	}
+	
+	@RequestMapping(value="/loginandcustomer/login.do", method = RequestMethod.POST)
+	public ModelAndView login(memberVO loginUserInfo,HttpServletRequest request) {
+		System.out.println("로그인하기위해 사용자입력값"+loginUserInfo);
+		ModelAndView mav = new ModelAndView();
+		memberVO loginuser = service.login(loginUserInfo);
+		System.out.println("로그인하고 나온정보"+loginuser);
+		mav.addObject("loginuser", loginuser);
+		String viewName="";
+		if(loginuser!=null) {
+			//로그인 성공시
+			HttpSession ses = request.getSession();
+			//2. 세션에 데이터 공유
+			ses.setAttribute("loginuser", loginuser);
+			viewName = "fridge";
+		}else {
+			//로그인 실패시 로그인 페이지 보여준다는 의미
+			viewName = "loginandcustomer/login.do";
+		}
+		mav.setViewName(viewName);//viewName을 변수처리.
+		return mav;
+	}
+	
+	@RequestMapping("/loginandcustomer/logout.do")
+	public String logout(HttpSession session) {
+		if(session!=null) {
+			session.invalidate();
+		}
+		return "redirect:/recipe/main.do";
+	}
+	
 
 	@RequestMapping("/loginandcustomer/join.do")
 	public String join() {
