f5c8de513bd24dd8eef7fe6ec9a278ea2de3a339
sjjam <shimjm21@gmail.com>
2020-02-07 10:01:11 +0900

    2/6 식당

--- maeggiSeggi/src/main/java/maeggi/seggi/restaurant/RestaurantController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/restaurant/RestaurantController.java
@@ -1,10 +1,18 @@
 package maeggi.seggi.restaurant;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RestaurantController {
+	@Autowired
+	RstService service;
 	@RequestMapping("/restaurant.do")
 	public String restaurantview() {
 		return "restaurant";
@@ -13,5 +21,14 @@ public class RestaurantController {
 	@RequestMapping("/click.do")
 	public String click() {
 		return "click";
+	}
+	
+	@RequestMapping(value="/restaurant/search.do",method=RequestMethod.GET,
+			produces="application/json;charset=utf-8")
+	public @ResponseBody ArrayList<searchVO> search(String tit) {
+		ArrayList<searchVO> searchlist = service.main(tit);
+		
+		System.out.println("SIZE : " + searchlist.size());
+		return searchlist;
 	}
 }
