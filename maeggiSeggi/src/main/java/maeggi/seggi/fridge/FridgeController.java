5ed01e66bf420a7543e95f55ea6f7bd83ddc30f7
So-Youn <soyun3963@gmail.com>
2020-02-11 15:49:34 +0900

    충돌 해결

--- maeggiSeggi/src/main/java/maeggi/seggi/fridge/FridgeController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/fridge/FridgeController.java
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-package maeggi.seggi.fridge;
-
-import java.util.ArrayList;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class FridgeController {
-	@Autowired
-	FridgeService service;
-	
-	@RequestMapping("/refrigerator/fridge.do")
-	public String fridgeContent() {
-		return "fridge";
-	}
-	
-	@RequestMapping(value="/refrigerator/ajax_fridge.do", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
-	public @ResponseBody ArrayList<FridgeVO> getUserFridge(String id) {
-		ArrayList<FridgeVO> list =  (ArrayList<FridgeVO>)service.selectMyFridgeByName(id);
-		return list;
-	}
-	
-	@RequestMapping("/calendar.do")
-	public String calendar() {
-		return "calendar";
-	}
-	
-	
-
-}
-=======
 package maeggi.seggi.fridge;
 
 import java.util.ArrayList;
@@ -80,4 +44,4 @@ public class FridgeController {
 	
 
 }
->>>>>>> refs/remotes/origin/master
+
