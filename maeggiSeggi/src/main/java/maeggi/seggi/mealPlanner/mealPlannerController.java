d3e1b51d19ff378899b8f6b65ab7eb94cdbd638e
tawn0414 <tawn0414@naver.com>
2020-02-12 22:02:40 +0900

    /* 주간 식단 개발중, 	top.jsp 식단관리 링크 수정

--- maeggiSeggi/src/main/java/maeggi/seggi/mealPlanner/mealPlannerController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/mealPlanner/mealPlannerController.java
@@ -2,6 +2,8 @@ package maeggi.seggi.mealPlanner;
 
 
 
+
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,37 +11,36 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import maeggi.seggi.loginandcustomer.memberVO;
-import maeggi.seggi.mypage.PointVO;
+
 
 @Controller
 public class mealPlannerController {
 	@Autowired
 	mealPlannerService service;
 	
-	/*@RequestMapping(value = "/mealPlanner/read.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/mealPlanner/select.do", method = RequestMethod.GET)
 	public ModelAndView read(mealPlannerVO meal,HttpServletRequest req) {
 		ModelAndView mav = new ModelAndView();
 		HttpSession ses = req.getSession(false);
 		if(ses!=null) {
 			memberVO user=(memberVO) ses.getAttribute("loginuser");
 			if(user!=null) {
-				meal.setMember_id((user.getMember_id()));//pointVO에 로그인한 회원의 id를 set.
+				meal.setMember_id((user.getMember_id()));//mealPlannerVO에 로그인한 회원의 id를 set.
 			}
 		}
-		mav.addAttribute("meal", service.read(meal));
-		return "mypage/main";
-	}*/
+		List<mealPlannerVO> mealplan = service.mealSelect(meal);
+		System.out.println("mealplan -==============="+mealplan);
+		mav.addObject("mealplan", mealplan);
+		mav.setViewName("mypage/main");
+		return mav;
+	}
+	
 
-	
-	
-	
-	
 	//식단에 추가하는 기능
 	@RequestMapping(value = "/mealPlanner/insert.do", method = RequestMethod.POST, produces="application/text;charset=UTF-8")
 	public void insert(mealPlannerVO meal) {
