cf9324a94d1f3e4426b065c5010d83d163d3e859
leeyj341 <leeyj3794@gmail.com>
2020-02-18 13:52:54 +0900

    * 레시피 등록

--- maeggiSeggi/src/main/java/maeggi/seggi/recipe/RecipeController.java
+++ maeggiSeggi/src/main/java/maeggi/seggi/recipe/RecipeController.java
@@ -289,13 +289,15 @@ public ModelAndView categoryList(String recipe_category, HttpServletRequest requ
 	 	
 	 	service.upload(file, path);
 	 	// 넣기 전 이미지 경로를 넣어줘야 함
-	 	recipe.setImg_url_main(file.get(0).getOriginalFilename());
-	 	for (int i = 0; i < recipe.getRecipe_detail().size(); i++) {
-			recipe.getRecipe_detail().get(i).setImg_url(file.get(i).getOriginalFilename());
-		}
+	 	recipe.setImg_url_main("/maeggiSeggi/uploadImages/" + file.get(0).getOriginalFilename());
+	 	if(file.size() > 1) {
+	 		for (int i = 0; i < recipe.getRecipe_detail().size(); i++) {
+				recipe.getRecipe_detail().get(i).setImg_url("/maeggiSeggi/uploadImages/" + file.get(i + 1).getOriginalFilename());
+			}
+	 	}
 	 	
 	 	service.insert(recipe);
-		return "forward:/recipe/recipe_write.do";
+		return "몰라";
 	}
 
 	@RequestMapping(value="/recipe/ajax_searchRecipe.do",method=RequestMethod.GET,produces="application/json;charset=utf-8")	
